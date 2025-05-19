4dc0fec464ed69bda91d4af55fe62cfa27455fb4
EEist23 <eimantas.endziulis@stud.svako.lt>
2025-05-19 10:43:26 +0300

    sutvarkyta prisijungimo/registracijos funkcija

--- contacts-app/routes/web.php
+++ contacts-app/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,13 +14,9 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-    
-    Route::prefix('c')->group(function () {
-        Route::get('/', [ContactController::class, 'index'])
-            ->name('contacts.index');
-        Route::get('/contacts/create', [ContactController::class, 'create'])
-            ->middleware('auth')->name('contacts.create');
-        Route::post('/contacts', [ContactController::class, 'store'])
-            ->middleware('auth')->name('contacts.store');
-    });
+});
+Route::prefix('c')->group(function () {
+Route::get('/', [ContactController::class, 'index'])->name('contacts.index');
+Route::get('/contacts/create', [ContactController::class, 'create'])->middleware('auth')->name('contacts.create');
+Route::post('/contacts', [ContactController::class, 'store'])->middleware('auth')->name('contacts.store');
 });
