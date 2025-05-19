8e2e65ae42c47746ffef94935cc560418010e215
EEist23 <eimantas.endziulis@stud.svako.lt>
2025-05-19 10:25:32 +0300

    pakeistas web.php kodas

--- contacts-app/routes/web.php
+++ contacts-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +15,13 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    
+    Route::prefix('c')->group(function () {
+        Route::get('/', [ContactController::class, 'index'])
+            ->name('contacts.index');
+        Route::get('/contacts/create', [ContactController::class, 'create'])
+            ->middleware('auth')->name('contacts.create');
+        Route::post('/contacts', [ContactController::class, 'store'])
+            ->middleware('auth')->name('contacts.store');
+    });
 });
