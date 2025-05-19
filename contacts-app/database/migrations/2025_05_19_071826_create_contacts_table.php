2e07fccc3f47de7874988f6943cabb7bbaf560d2
EEist23 <eimantas.endziulis@stud.svako.lt>
2025-05-19 10:20:36 +0300

    pakeistas database/migrations/create_contacts_table.php

--- contacts-app/database/migrations/2025_05_19_071826_create_contacts_table.php
+++ contacts-app/database/migrations/2025_05_19_071826_create_contacts_table.php
@@ -1,27 +1,24 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('contacts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+Route::get('/', function () {
+    return view('welcome');
+});
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('contacts');
-    }
-};
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::prefix('c')->group(function () {
+    Route::get('/', [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/create', [ContactController::class, 'create'])->middleware('auth')->name('contacts.create');
+    Route::post('/', [ContactController::class, 'store'])->middleware('auth')->name('contacts.store');
+});
