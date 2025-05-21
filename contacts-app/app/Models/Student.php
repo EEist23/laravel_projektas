9565bd572bb1bf213e5468e057803b02450cf86c
EEist23 <eimantas.endziulis@stud.svako.lt>
2025-05-21 09:38:02 +0300

    atnaujintas student modelis

--- contacts-app/app/Models/Student.php
+++ contacts-app/app/Models/Student.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'surname', 'address', 'phone', 'city_id'];
+
+    // Susiejimas su miestu
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
 }
