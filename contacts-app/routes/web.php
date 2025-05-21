645d685e369e567c04de2d6834e1791d3603ea53
EEist23 <eimantas.endziulis@stud.svako.lt>
2025-05-21 10:00:08 +0300

    Redaguotas web.php

--- contacts-app/routes/web.php
+++ contacts-app/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\StudentController;
+
+Route::resource('students', StudentController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
