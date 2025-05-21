91ab24aabaa9f90106559ec7f0713c49ee25f6af
EEist23 <eimantas.endziulis@stud.svako.lt>
2025-05-21 10:05:27 +0300

    redaguota layout.blade.php, index.blade.php ir create_students_table

--- contacts-app/database/migrations/2025_05_21_063145_create_students_table.php
+++ contacts-app/database/migrations/2025_05_21_063145_create_students_table.php
@@ -4,23 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStudentsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+
+    public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('vardas');
-            $table->string('pavarde');
-            $table->date('gim_data');
-            $table->string('telefonas');
-            $table->text('adresas');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('address');
+            $table->string('phone');
             $table->timestamps();
+
         });
     }
-    
-    public function down()
+
+    public function down(): void
     {
         Schema::dropIfExists('students');
     }
-}
+};
