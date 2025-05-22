cbaa6acc55b4ec7e3c89e5fa751363cc86d1219f
EEist23 <eimantas.endziulis@stud.svako.lt>
2025-05-22 10:34:52 +0300

    redaguota web.php ir veikia email išsiuntimo funkcija

--- contacts-app/routes/web.php
+++ contacts-app/routes/web.php
@@ -4,12 +4,21 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\PdfController;
+use Illuminate\Support\Facades\Mail;
+
+Route::get('/test-email', function () {
+    Mail::raw('Tai yra testinis laiškas!', function ($message) {
+        $message->to('eimantas.endziulis@stud.svako.lt')
+                ->subject('Testas iš Laravel');
+    });
+
+    return 'Laiškas išsiųstas!';
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('/generate-pdf', [PDFController::class, 'generatePDF']);
 Route::get('students/generate-pdf', [PDFController::class, 'generatePDF']);
 
 Route::get('/dashboard', function () {
@@ -22,30 +31,13 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-/*Route::get('students/trashed', [StudentController::class, 'trashed'])->name('students.trashed');
-
-
-Route::post('students/{id}/restore', [StudentController::class, 'restore'])->name('students.restore');
-Route::delete('students/{id}/forceDelete', [StudentController::class, 'forceDelete'])->name('students.forceDelete');
-Route::resource('students', StudentController::class);
-*/
-
-
-
-// Studento sąrašas prieinamas visiems
 Route::get('students', [StudentController::class, 'index'])->name('students.index');
 
-// Visi kiti studentų maršrutai reikalauja autentifikacijos
 Route::middleware('auth')->group(function () {
-
     Route::get('students/trashed', [StudentController::class, 'trashed'])->name('students.trashed');
     Route::post('students/{id}/restore', [StudentController::class, 'restore'])->name('students.restore');
     Route::delete('students/{id}/forceDelete', [StudentController::class, 'forceDelete'])->name('students.forceDelete');
     Route::resource('students', StudentController::class)->except(['index', 'show']);
 });
-
-
-
-
 
 require __DIR__.'/auth.php';
